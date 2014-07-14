273734ed4a50860273d17da06a98673865905a7e
Ashley Penney <ashley.penney@puppetlabs.com>
2014-07-14 15:57:40 -0400

    Add f5_monitor{}.

---
+++ lib/puppet/type/f5_monitor.rb
@@ -0,0 +1,165 @@
+require 'puppet/parameter/name'
+require 'puppet/property/description'
+
+Puppet::Type.newtype(:f5_monitor) do
+  @doc = 'Manage monitor objects'
+
+  apply_to_device
+  ensurable
+
+  newparam(:name, :parent => Puppet::Parameter::F5Name, :namevar => true)
+  newproperty(:description, :parent => Puppet::Property::F5Description)
+
+  newproperty(:interval) do
+    validate do |value|
+      fail ArgumentError, "Valid options: #{options}" unless value =~ /^\d+$/
+    end
+  end
+
+  newproperty(:up_interval) do
+    newvalues(:disabled, :enabled, :true, :false)
+  end
+
+  newproperty(:time_until_up) do
+    validate do |value|
+      fail ArgumentError, "Valid options: #{options}" unless value =~ /^\d+$/
+    end
+  end
+
+  newproperty(:timeout) do
+    validate do |value|
+      fail ArgumentError, "Valid options: #{options}" unless value =~ /^\d+$/
+    end
+  end
+
+  newproperty(:manual_resume) do
+    newvalues(:yes, :no, :true, :false)
+  end
+
+  newproperty(:send_string) do
+  end
+
+  newproperty(:receive_string) do
+  end
+
+  newproperty(:receive_disable_string) do
+  end
+
+  newproperty(:cipher_list) do
+  end
+
+  newproperty(:user) do
+  end
+
+  newproperty(:password) do
+  end
+
+  newproperty(:compatibility) do
+    newvalues(:disabled, :enabled, :true, :false)
+  end
+
+  newproperty(:client_certificate) do
+  end
+
+  newproperty(:client_key) do
+  end
+
+  newproperty(:reverse) do
+    newvalues(:yes, :no, :true, :false)
+  end
+
+  newproperty(:transparent) do
+    newvalues(:yes, :no, :true, :false)
+  end
+
+  newproperty(:alias_address) do
+    options = '<ipv4|ipv6>'
+    desc "The IP address of the resource.
+    Valid options: #{options}"
+
+    validate do |value|
+      unless value.match(Resolv::IPv6::Regex) || value.match(Resolv::IPv4::Regex) || value =~ /^\*$/
+        fail ArgumentError, "#{name} must be: #{options}."
+      end
+    end
+  end
+
+  newproperty(:alias_service_port) do
+    validate do |value|
+      fail ArgumentError, "Valid options: #{options}" unless value =~ /^(\*|\d+)$/
+      # Only check in the case of a number.
+      if value =~ /\d+$/
+        fail ArgumentError, "Alias_service_port:  Must be between 1-65535" unless value.to_i.between?(1,65535)
+      end
+    end
+  end
+
+  newproperty(:ip_dscp) do
+    validate do |value|
+      fail ArgumentError, "Valid options: #{options}" unless value =~ /^\d+$/
+    end
+  end
+
+  newproperty(:debug) do
+    newvalues(:yes, :no, :true, :false)
+  end
+
+  newproperty(:base) do
+  end
+
+  newproperty(:filter) do
+  end
+
+  newproperty(:security) do
+    newvalues(:none, :ssl, :tls)
+  end
+
+  newproperty(:mandatory_attributes) do
+    newvalues(:yes, :no, :true, :false)
+  end
+
+  newproperty(:chase_referrals) do
+    newvalues(:yes, :no, :true, :false)
+  end
+
+  newproperty(:external_program) do
+  end
+
+  newproperty(:arguments) do
+  end
+
+  # Validate it's an array globally.
+  newproperty(:variables) do
+    #validate do |value|
+    #  fail ArgumentError, "Variables must be a hash of key=>value pairs." unless value.is_a?(Hash)
+    #end
+  end
+
+  newproperty(:mode) do
+    newvalues(:tcp, :udp, :tls, :sips)
+  end
+
+  newproperty(:additional_accepted_status_codes, :array_matching => :all) do
+    validate do |value|
+      unless ['any', 'none'].include?(value) or value =~ /\d+/
+        fail ArgumentError, "Valid options: #{options}"
+      end
+    end
+  end
+
+  newproperty(:additional_rejected_status_codes, :array_matching => :all) do
+    validate do |value|
+      unless ['any', 'none'].include?(value) or value =~ /\d+/
+        fail ArgumentError, "Valid options: #{options}"
+      end
+    end
+  end
+
+  # Validate it's an array globally.
+  newproperty(:header_list, :array_matching => :all) do
+  end
+
+  newproperty(:sip_request) do
+  end
+
+end
---
+++ spec/unit/type/f5_monitor_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:f5_monitor) do
+
+  before :each do
+    @node = Puppet::Type.type(:f5_monitor).new(:name => '/Common/testing')
+  end
+
+  {
+    'interval'                         => { pass: %w(0 1 20 254 255), fail: %w(-1 true false enabled word 0.15)},
+    'up_interval'                      => { pass: %w(disabled enabled true false), fail: %w(yes please magic present superenabled) },
+    'time_until_up'                    => { pass: %w(0 1 20 254 255), fail: %w(-1 true false enabled word 0.15)},
+    'timeout'                          => { pass: %w(0 1 20 254 255), fail: %w(-1 true false enabled word 0.15)},
+    'manual_resume'                    => { pass: %w(yes no true false), fail: %w(disabled enabled please magic present superenabled) },
+    'compatibility'                    => { pass: %w(disabled enabled true false), fail: %w(1 2 3 4 5 yes no) },
+    'reverse'                          => { pass: %w(yes no true false), fail: %w(disabled enabled please magic present superenabled) },
+    'transparent'                      => { pass: %w(yes no true false), fail: %w(disabled enabled please magic present superenabled) },
+    'alias_address'                    => { pass: %w(10.1.1.1 192.168.1.1 4.2.2.2 2001:cdba:0000:0000:0000:0000:3257:9652), fail: %w(-1 256 true false enabled word 0.15) },
+    'alias_service_port'               => { pass: %w(* 1 10 20 65535), fail: %w(-1 65536 mimic true false enabled word 0.15) },
+    'ip_dscp'                          => { pass: %w(0 10 200 3000), fail: %w(-1 mimic true false enabled word 0.15) },
+    'debug'                            => { pass: %w(yes no true false), fail: %w(disabled enabled please magic present superenabled) },
+    'security'                         => { pass: %w(none ssl tls), fail: %w(disabled enabled please magic present superenabled) },
+    'mandatory_attributes'             => { pass: %w(yes no true false), fail: %w(disabled enabled please magic present superenabled) },
+    'chase_referrals'                  => { pass: %w(yes no true false), fail: %w(disabled enabled please magic present superenabled) },
+    'mode'                             => { pass: %w(tcp udp tls sips), fail: %w(disabled enabled please magic present superenabled) },
+    'additional_accepted_status_codes' => { pass: %w(any none 20 300 400 5000), fail: %w(true false enabled word ) },
+    'additional_rejected_status_codes' => { pass: %w(any none 20 300 400 5000), fail: %w(true false enabled word ) },
+  }.each do |test, states|
+    describe "#{test}" do
+      states[:pass].each do |item|
+        it "should allow #{test} to be set to #{item}" do
+          @node[test.to_sym] = item
+          # Special case the arrays we expect here.
+          if ['additional_accepted_status_codes', 'additional_rejected_status_codes'].include?(test)
+            expect(@node[test.to_sym]).to eq(Array(item))
+          else
+            expect(@node[test.to_sym].to_sym).to eq(item.to_sym)
+          end
+        end
+      end
+
+      states[:fail].each do |item|
+        it "should fail when #{test} is set to #{item}" do
+          expect { @node[test.to_sym] = item }.to raise_error()
+        end
+      end
+    end
+  end
+
+end
