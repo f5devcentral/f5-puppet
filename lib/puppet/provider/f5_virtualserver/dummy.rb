6cac89f3ef1bc00e42f0ccc2299031e37c6a5be3
Hunter Haugen <hunter@puppetlabs.com>
2014-10-20 16:53:59 -0700

    Add ability to bail without provider passed (hacky)

---
+++ lib/puppet/provider/f5_virtualserver/dummy.rb
@@ -0,0 +1,15 @@
+require 'puppet/provider/f5'
+
+Puppet::Type.type(:f5_virtualserver).provide(:dummy, :parent => Puppet::Provider::F5) do
+  desc "Dummy default provider"
+
+  defaultfor :feature => :f5_device
+  def self.instances
+    []
+  end
+
+  def exists?
+    providers = @resource.class.providers.map{|x| x.to_s}.sort.reject{|x| x == "dummy"}.join(", ") rescue "none"
+    raise("f5_virtualserver requires that a provider is declared. Available providers are: #{providers}")
+  end
+end
