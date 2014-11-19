732a37355c929c843c194e617dde02ae3e64e881
Hunter Haugen <hunter@puppetlabs.com>
2014-11-19 10:07:28 -0800

    Fix f5 feature for puppet resource

--- lib/puppet/feature/f5_device.rb
+++ lib/puppet/feature/f5_device.rb
@@ -1,10 +1,17 @@
 require 'puppet/util/feature'
-require 'puppet/util/network_device/f5/transport'
+require 'puppet/util/network_device/transport/f5'
 require 'puppet/util/network_device/f5/facts'
 
 Puppet.features.add(:f5_device) do
   begin
-    transport = Puppet::Util::NetworkDevice::F5::Transport.new(Facter.value(:url))
+    transport = nil
+    if Puppet::Util::NetworkDevice.current
+      #we are in `puppet device`
+      transport = Puppet::Util::NetworkDevice.current.transport
+    else
+      #we are in `puppet resource`
+      transport = Puppet::Util::NetworkDevice::Transport::F5.new(Facter.value(:url))
+    end
     facts     = Puppet::Util::NetworkDevice::F5::Facts.new(transport).retrieve
     if facts and facts[:operatingsystem] == :F5
       true
