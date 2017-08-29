63ec28e269b6ecd8aaf83b2bb3cca40e209d2300
ericzji <zhaohui_ji@yahoo.com>
2017-08-28 17:25:54 -0700

    Add f5_configsync resource

---
+++ lib/puppet/type/f5_configsync.rb
@@ -0,0 +1,42 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/parameter/f5_name.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_address.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_availability_requirement.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_connection_limit.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_connection_rate_limit.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_description.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_health_monitors.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_ratio.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_state.rb'))
+
+Puppet::Type.newtype(:f5_configsync) do
+  @doc = 'Run tmsh command'
+
+  apply_to_device
+  ensurable
+
+  newparam(:name) do
+    def self.postinit
+      @doc ||= "The name of the object.
+      Valid options: <String>"
+    end
+
+    validate do |value|
+      fail ArgumentError, "#{name} must be a String" unless value.is_a?(String)
+    end
+
+    isnamevar
+
+  end
+
+  newproperty(:description, :parent => Puppet::Property::F5Description)
+
+  newproperty(:to_group) do
+    desc "to_group"
+  end
+
+  newproperty(:tmsh) do
+    desc "tmsh"
+    # TODO: Should we validate this?
+  end
+
+end
