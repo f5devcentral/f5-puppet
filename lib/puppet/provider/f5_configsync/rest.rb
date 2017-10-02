2b8b9a0deaf6ad1d90e65e7273f4fc556e996c84
Eric Ji <zhaohui_ji@yahoo.com>
2017-10-02 15:40:56 -0700

    Removed comments

--- lib/puppet/provider/f5_configsync/rest.rb
+++ lib/puppet/provider/f5_configsync/rest.rb
@@ -3,15 +3,6 @@ require 'json'
 
 Puppet::Type.type(:f5_configsync).provide(:rest, parent: Puppet::Provider::F5) do
 
-#  def self.instances
-#    instances = []
-#    return []
-#  end
-
-#  def self.prefetch(resources)
-#    nodes = instances
-#  end
-
   def create_message(basename, hash)
     # Create the message by stripping :present.
     new_hash            = hash.reject { |k, _| [:ensure,:name, :provider, Puppet::Type.metaparams].flatten.include?(k) }
@@ -35,15 +26,6 @@ Puppet::Type.type(:f5_configsync).provide(:rest, parent: Puppet::Provider::F5) d
    message.to_json
   end
 
-#  def flush
-#    if @property_hash != {}
-#      # You can only pass address to create, not modifications.
-#      flush_message = @property_hash.reject { |k, _| k == :address }
-#      result = Puppet::Provider::F5.put("/mgmt/tm/cm/config-sync", message(flush_message))
-#    end
-#    return result
-#  end
-
   def exists?
     @property_hash[:ensure] == :present
   end
@@ -55,13 +37,6 @@ Puppet::Type.type(:f5_configsync).provide(:rest, parent: Puppet::Provider::F5) d
 
     return result
   end
-
-#  def destroy
-#    result = Puppet::Provider::F5.delete("/mgmt/tm/cm/config-sync")
-#    @property_hash.clear
-#
-#    return result
-#  end
 
   mk_resource_methods
 
