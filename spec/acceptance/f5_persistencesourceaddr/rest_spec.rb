5e53b5373478bff9ac12d25cda6aa3a97b2e1af5
ericzji <zhaohui_ji@yahoo.com>
2017-10-06 09:28:42 -0700

    Acceptance test for f5_persistencesourceaddr

---
+++ spec/acceptance/f5_persistencesourceaddr/rest_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper_acceptance'
+
+describe 'f5_persistencesourceaddr' do
+  it 'creates and updates f5_persistencesourceaddr' do
+    pp=<<-EOS
+    f5_persistencesourceaddr { '/Common/source_addr1':
+      ensure                     => 'present',
+      match_across_pools         => 'enabled',
+      match_across_services      => 'enabled',
+      match_across_virtuals      => 'enabled',
+      hash_algorithm             => 'carp',
+      mask                       => '255.255.0.0',
+      timeout                    => '180',
+      override_connection_limit  => 'enabled',
+    }
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+
+    pp2=<<-EOS
+    f5_persistencesourceaddr { '/Common/source_addr1':
+      ensure                     => 'present',
+      match_across_pools         => 'disabled',
+      match_across_services      => 'disabled',
+      match_across_virtuals      => 'disabled',
+      hash_algorithm             => 'default',
+      mask                       => '255.255.255.0',
+      timeout                    => '100',
+      override_connection_limit  => 'disabled',
+    }
+    EOS
+    make_site_pp(pp2)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)    
+  end
+
+  it 'deletes f5_persistencesourceaddr' do
+    pp=<<-EOS
+    f5_persistencesourceaddr { '/Common/source_addr1':
+      ensure => 'absent',
+    }    
+    EOS
+    make_site_pp(pp)
+    run_device(:allow_changes => true)
+    run_device(:allow_changes => false)
+
+  end
+end
+
